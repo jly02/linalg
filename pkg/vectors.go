6ff00c750e15b1d3780d9797aaf985c1605ed849
jly02 <lyjonathan02@gmail.com>
2022-08-23 13:22:58 -0700

    Genericized parameters and implemented scalar multiplication

--- pkg/vectors.go
+++ pkg/vectors.go
@@ -8,15 +8,15 @@ import "errors"
 // Computes the dot product of two vectors.
 //
 // A successful Dot() call will return err == nil.
-func Dot(first []int, second []int) (int, error) {
+func Dot[T int | float64 | float32](first []T, second []T) (float64, error) {
 	// Check if lengths are equal
 	if len(first) != len(second) {
-		return 0, errors.New("length of each array must be equal")
+		return 0, errors.New("length of each vector must be equal")
 	}
 
-	var dot int = 0
+	var dot float64 = 0
 	for i := 0; i < len(first); i++ {
-		dot += first[i] * second[i]
+		dot += float64(first[i]) * float64(second[i])
 	}
 
 	return dot, nil
@@ -25,15 +25,35 @@ func Dot(first []int, second []int) (int, error) {
 // Adds two vectors, returning the resulting vector.
 //
 // A successful Add() call will return err == nil.
-func Add(first []int, second []int) ([]int, error) {
+func Add[T int | float64 | float32](first []T, second []T) ([]float64, error) {
 	// Check if lengths are equal
 	if len(first) != len(second) {
-		return nil, errors.New("length of each array must be equal")
+		return nil, errors.New("length of each vector must be equal")
 	}
 
-	res := make([]int, len(first))
+	res := make([]float64, len(first))
 	for i := 0; i < len(first); i++ {
-		res[i] = first[i] + second[i]
+		res[i] = float64(first[i]) + float64(second[i])
+	}
+
+	return res, nil
+}
+
+// Multiplies a given vector by a scalar value.
+//
+// A successful ScalarMult() call will return err == nil.
+func ScalarMult[T int | float64 | float32](vec []T, scalar float64) ([]float64, error) {
+	if len(vec) < 1 {
+		return nil, errors.New("vector cannot be empty")
+	}
+
+	res := make([]float64, len(vec))
+	if scalar == 0 {
+		return res, nil
+	}
+
+	for i := 0; i < len(vec); i++ {
+		res[i] = float64(vec[i]) * scalar
 	}
 
 	return res, nil
--- test/vectors_test.go
+++ test/vectors_test.go
@@ -11,10 +11,10 @@ func TestDotWithNil(t *testing.T) {
 	var first []int = nil
 	var second []int = nil
 
-	want := 0
+	want := 0.
 	dot, err := vectors.Dot(first, second)
 	if err != nil || dot != want {
-		t.Fatalf(`Dot([], []) = %d, %v, want 0`, dot, err)
+		t.Fatalf(`Dot([], []) = %f, %v, want 0`, dot, err)
 	}
 }
 
@@ -22,10 +22,10 @@ func TestDotWithEmpty(t *testing.T) {
 	first := make([]int, 0)
 	second := make([]int, 0)
 
-	want := 0
+	want := 0.
 	dot, err := vectors.Dot(first, second)
 	if err != nil || dot != want {
-		t.Fatalf(`Dot([0], [0]) = %d, %v, want 0`, dot, err)
+		t.Fatalf(`Dot([0], [0]) = %f, %v, want 0`, dot, err)
 	}
 }
 
@@ -33,10 +33,10 @@ func TestDotWithOne(t *testing.T) {
 	first := []int{5}
 	second := []int{10}
 
-	want := 50
+	want := 50.
 	dot, err := vectors.Dot(first, second)
 	if err != nil || dot != want {
-		t.Fatalf(`Dot([1], [1]) = %d, %v, want 0`, dot, err)
+		t.Fatalf(`Dot([1], [1]) = %f, %v, want 0`, dot, err)
 	}
 }
 
@@ -44,10 +44,10 @@ func TestDotWithFew(t *testing.T) {
 	first := []int{1, 2, 3}
 	second := []int{4, 5, 6}
 
-	want := 32
+	want := 32.
 	dot, err := vectors.Dot(first, second)
 	if err != nil || dot != want {
-		t.Fatalf(`Dot([3], [3]) = %d, %v, want 0`, dot, err)
+		t.Fatalf(`Dot([3], [3]) = %f, %v, want 0`, dot, err)
 	}
 }
 
@@ -55,10 +55,10 @@ func TestDotWithMany(t *testing.T) {
 	first := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 	second := []int{11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191, 201}
 
-	want := 28910
+	want := 28910.
 	dot, err := vectors.Dot(first, second)
 	if err != nil || dot != want {
-		t.Fatalf(`Dot([20], [20]) = %d, %v, want 0`, dot, err)
+		t.Fatalf(`Dot([20], [20]) = %f, %v, want 0`, dot, err)
 	}
 }
 
@@ -66,10 +66,10 @@ func TestDotWithNegative(t *testing.T) {
 	first := []int{-1, -2, 3}
 	second := []int{-3, 2, 4}
 
-	want := 11
+	want := 11.
 	dot, err := vectors.Dot(first, second)
 	if err != nil || dot != want {
-		t.Fatalf(`Dot([3], [3]) = %d, %v, want 0`, dot, err)
+		t.Fatalf(`Dot([3], [3]) = %f, %v, want 0`, dot, err)
 	}
 }
 
@@ -79,7 +79,7 @@ func TestDotWithDifferentLengths(t *testing.T) {
 
 	dot, err := vectors.Dot(first, second)
 	if err == nil {
-		t.Fatalf(`Dot([5], [4]) = %d did not return error as expected`, dot)
+		t.Fatalf(`Dot([5], [4]) = %f, did not return error as expected`, dot)
 	}
 }
 
@@ -89,7 +89,7 @@ func TestAddWithNil(t *testing.T) {
 
 	vec, err := vectors.Add(first, second)
 	if err != nil {
-		t.Fatalf(`Add(nil, nil) = %d returned error`, vec)
+		t.Fatalf(`Add(nil, nil) = %f returned error`, vec)
 	}
 }
 
@@ -97,10 +97,10 @@ func TestAddWithOne(t *testing.T) {
 	first := []int{1}
 	second := []int{2}
 
-	want := 3
+	want := []float64{3}
 	vec, err := vectors.Add(first, second)
-	if err != nil || vec[0] != want || len(vec) > 1 {
-		t.Fatalf(`Add([1], [1]) = {%d}, want {%d}`, vec[0], want)
+	if err != nil || !reflect.DeepEqual(want, vec) {
+		t.Fatalf(`Add([1], [1]) = {%f}, want {%f}`, vec[0], want)
 	}
 }
 
@@ -108,10 +108,10 @@ func TestAddWithFew(t *testing.T) {
 	first := []int{1, 2, 3}
 	second := []int{4, 5, 6}
 
-	want := []int{5, 7, 9}
+	want := []float64{5, 7, 9}
 	vec, err := vectors.Add(first, second)
 	if err != nil || !reflect.DeepEqual(vec, want) {
-		t.Fatalf(`Add([3], [3]) = {%d}, want {%d}`, vec, want)
+		t.Fatalf(`Add([3], [3]) = {%f}, want {%f}`, vec, want)
 	}
 }
 
@@ -119,10 +119,10 @@ func TestAddWithMany(t *testing.T) {
 	first := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
 	second := []int{20, 18, 16, 14, 12, 10, 8, 6, 4, 2}
 
-	want := []int{22, 22, 22, 22, 22, 22, 22, 22, 22, 22}
+	want := []float64{22, 22, 22, 22, 22, 22, 22, 22, 22, 22}
 	vec, err := vectors.Add(first, second)
 	if err != nil || !reflect.DeepEqual(vec, want) {
-		t.Fatalf(`Add([10], [10]) = {%d}, want {%d}`, vec, want)
+		t.Fatalf(`Add([10], [10]) = {%f}, want {%f}`, vec, want)
 	}
 }
 
@@ -130,10 +130,10 @@ func TestAddWithNegative(t *testing.T) {
 	first := []int{-1, -2, -3}
 	second := []int{3, 2, -1}
 
-	want := []int{2, 0, -4}
+	want := []float64{2, 0, -4}
 	vec, err := vectors.Add(first, second)
 	if err != nil || !reflect.DeepEqual(vec, want) {
-		t.Fatalf(`Add([3], [3]) = {%d}, want {%d}`, vec, want)
+		t.Fatalf(`Add([3], [3]) = {%f}, want {%f}`, vec, want)
 	}
 }
 
@@ -143,6 +143,71 @@ func TestAddWithDifferentLengths(t *testing.T) {
 
 	vec, err := vectors.Add(first, second)
 	if err == nil {
-		t.Fatalf(`Add([5], [4]) = %d did not return error as expected`, vec)
+		t.Fatalf(`Add([5], [4]) = %f, did not return error as expected`, vec)
+	}
+}
+
+func TestScalarMultWithNil(t *testing.T) {
+	var vec []int = nil
+	scalar := 0.
+
+	res, err := vectors.ScalarMult(vec, scalar)
+	if err == nil {
+		t.Fatalf(`ScalarMult(nil, 0) = %f, did not return error as expected`, res)
+	}
+}
+
+func TestScalarMultWithZeroScalar(t *testing.T) {
+	vec := []int{1, 2, 3}
+	scalar := 0.
+
+	want := []float64{0., 0., 0.}
+	res, err := vectors.ScalarMult(vec, scalar)
+	if err != nil || !reflect.DeepEqual(res, want) {
+		t.Fatalf(`ScalarMult([3], 0) = {%f}, want {%f}`, res, want)
+	}
+}
+
+func TestScalarMultWithOne(t *testing.T) {
+	vec := []int{1}
+	scalar := 3.
+
+	want := []float64{3.}
+	res, err := vectors.ScalarMult(vec, scalar)
+	if err != nil || !reflect.DeepEqual(res, want) {
+		t.Fatalf(`ScalarMult([1], 3) = {%f}, want {%f}`, res, want)
+	}
+}
+
+func TestScalarMultWithMany(t *testing.T) {
+	vec := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	scalar := 3.
+
+	want := []float64{3., 6., 9., 12., 15., 18., 21., 24., 27., 30.}
+	res, err := vectors.ScalarMult(vec, scalar)
+	if err != nil || !reflect.DeepEqual(res, want) {
+		t.Fatalf(`ScalarMult([10], 3) = {%f}, want {%f}`, res, want)
+	}
+}
+
+func TestScalarMultWithNegative(t *testing.T) {
+	vec := []int{1, 3, -4}
+	scalar := -2.
+
+	want := []float64{-2, -6, 8}
+	res, err := vectors.ScalarMult(vec, scalar)
+	if err != nil || !reflect.DeepEqual(res, want) {
+		t.Fatalf(`ScalarMult([3], -2) = {%f}, want {%f}`, res, want)
+	}
+}
+
+func TestScalarMultWithNonInteger(t *testing.T) {
+	vec := []int{1, 2, 3, 4}
+	scalar := .5
+
+	want := []float64{.5, 1., 1.5, 2.}
+	res, err := vectors.ScalarMult(vec, scalar)
+	if err != nil || !reflect.DeepEqual(res, want) {
+		t.Fatalf(`ScalarMult([3], -2) = {%f}, want {%f}`, res, want)
 	}
 }
